980e14d3c18d83ba2946b64ee6619ad28f4fd51b
富山涼平 <tennisnowboarderyou@gmail.com>
2018-10-20 16:54:38 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## swagger-multi-file-docker
+### 概要
+ファイル分割さらたswagger定義ファイルを編集しつつ、swagger-uiで表示
+
+### 起動
+
+```
+$ docker-compose build
+$ docker-compose up -d
+```
+
+https://localhost:8000
+
---
+++ swagger-watch/index.js
@@ -0,0 +1,34 @@
+const { Gaze } = require('gaze');
+const path = require('path');
+const merger = require('swagger-merger');
+
+function resolve_path (dir) {
+  return path.join(__dirname, '..', dir);
+}
+
+function multi_file() {
+  const file_path = resolve_path('./src/index.yml');
+  const dest_file_path = resolve_path('./docs/swagger.yml');
+  const option = {
+    input: file_path,
+    output: dest_file_path,
+  };
+  merger(option).catch(err => {
+    console.error(err);
+  });
+}
+function watch() {
+  const gaze = new Gaze(resolve_path('./src/**'));
+  gaze.on('error', (err) => { throw err; });
+  gaze.on('all', (event, file) => {
+    console.log(event, file);
+    multi_file();
+  });
+  const restart = () => {
+    gaze.close();
+    watch();
+  }
+  gaze.on('added', (watcher) => restart());
+  gaze.on('removed', (watcher) => restart());
+}
+watch();
